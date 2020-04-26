1580f684c66ccdcd8abe642865adfaf3cfe97ff9
Evan Berg <evan.m.berg@gmail.com>
2020-04-25 19:10:56 -0500

    moved social icons down

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,12 @@
 */
 
 (function($) {
+	$(document).ready(function(){
+	  console.log('Well then get your shit together, get it all together and put it in a back pack, all your shit, so it\'s together.');
+	  console.log('And if you gotta take it some where, take it somewhere, you know, take it to the shit store and sell it, or put it in the shit museum. I don\'t care what you do, you just gotta get it together.');
+	  console.log('Get your shit together.');
+	});
+
 
 	var	$window = $(window),
 		$body = $('body'),
